422f4fec5c9303d0c112dad1e59dc233f8c7a0c9
Zachary Kirchin <zacharykirchin@gmail.com>
2015-09-06 20:25:38 -0400

    deployment configuration

--- config.rb
+++ config.rb
@@ -1,6 +1,10 @@
-# To deploy to a remote branch via git (e.g. gh-pages on github):
 activate :deploy do |deploy|
-  deploy.method = :git
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
 
 
